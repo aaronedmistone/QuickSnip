59573f47c7f92aeb97d178d64db99d1258242e5d
aaronedmistone <aaron.edmistone@gmail.com>
2020-06-19 01:12:17 +1000

    Initial commit

--- QuickSnip/Properties/AssemblyInfo.cs
+++ QuickSnip/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("QuickSnip")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple snip tool that saves the capture automatically and copies it to the clipboard.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Aaron Edmistone")]
 [assembly: AssemblyProduct("QuickSnip")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © Aaron Edmistone 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
